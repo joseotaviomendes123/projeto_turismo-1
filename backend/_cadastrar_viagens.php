485a0baec80a3868b6c7787fa9a8d08df301dffc
MateusMAART <101677318+MatteusMart@users.noreply.github.com>
2022-08-03 21:11:07 -0300

    backend php adicionado

---
+++ backend/_cadastrar_viagens.php
@@ -0,0 +1,51 @@
+<?php
+
+
+// Include da conexão com o banco de dados
+include 'conexao.php';
+
+try{
+    // exibir as váriaveis globais recebidas via POST
+    //echo "<pre>";
+
+    //var_dump($_POST);
+    //echo "</pre>";
+
+    // variaveis que recebem os dados enviados via POST
+    $titulo = $_POST['título'];
+    $local = $_POST['local'];
+    $Valor = $_POST['Valor'];
+    $desc = $_POST['desc'];
+
+    // variaveis que recebe a query SQL que será executada no BD
+    $sql = "INSERT INTO
+                tb_viagens
+            (
+                `titulo`,
+                `local`,
+                `valor`,
+                `desc`
+            )
+            VALUES
+            (
+                '$titulo',
+                '$local',
+                '$Valor',
+                '$desc'
+            )    
+            ";
+
+    // prepara a execucão da query SQL acima
+    $comando = $con->prepare($sql);
+
+    $comando -> execute();
+    
+    // exibe msg de sucesso ao inserir
+    echo "cadastro realizado com sucesso";
+
+
+}catch(PDOException $erro){
+
+}
+
+?>
---
+++ backend/conexao.php
@@ -0,0 +1,20 @@
+<?php
+
+try{
+     // dados da conexão com o BD
+     define('SERVIDOR','localhost');
+     define('USUARIO','root');
+     define('SENHA','');
+     define('BASEDADOS','db_turismo');
+
+     $con = new PDO("mysql:host=".SERVIDOR.";dbname=".BASEDADOS, USUARIO, SENHA);
+     // set the PDO error mode to exception
+     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //  echo "Conectado com sucesso";
+
+
+}catch(PDOException $erro){
+    echo "Erro ao conectar no banco de dados: ".$erro->getMessage();
+
+
+}
