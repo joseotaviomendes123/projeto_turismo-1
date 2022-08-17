265c730028fb02e9ca2c74d83faccae580d86547
joseotaviomendes123 <joseotaviomendesdacosta@gmail.com>
2022-08-17 20:09:37 -0300

    senha case sensitive add

--- backend/_validar_login.php
+++ backend/_validar_login.php
@@ -6,7 +6,7 @@ try{
     $usuario = $_POST['usuario'];
     $senha = $_POST['senha'];
 
-    $sql = "SELECT * FROM tb_login WHERE email='$usuario' AND senha='$senha'";
+    $sql = "SELECT * FROM tb_login WHERE email='$usuario' AND BINARY senha='$senha'";
 
     $comando = $con->prepare($sql);
 
